5306c5cc9acfe7ed8ca5e213ac0576b308525558
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2026-02-18 12:52:53 +0000

    8078744: Right half of system menu icon on title bar does not activate when clicked in Metal L&F

--- src/java.desktop/share/classes/javax/swing/plaf/metal/MetalTitlePane.java
+++ src/java.desktop/share/classes/javax/swing/plaf/metal/MetalTitlePane.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -436,7 +436,7 @@ class MetalTitlePane extends JComponent {
      * for manipulating the Frame.
      */
     private JMenu createMenu() {
-        JMenu menu = new JMenu("");
+        JMenu menu = new JMenu(" ");
         if (getWindowDecorationStyle() == JRootPane.FRAME) {
             addMenuItems(menu);
         }
